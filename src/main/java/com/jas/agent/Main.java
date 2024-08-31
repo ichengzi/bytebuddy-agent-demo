f6b067e31a110bfdd11a534fd4a6fb5867c29d77
ReaJason <reajason1225@gmail.com>
2024-08-31 12:25:26 +0800

    fix: rm dump folder
    resolved #14
    suitable for 2024.6 and idea 2024.2+ closed #13,closed #12,closed #11, closed # 10

--- src/main/java/com/jas/agent/Main.java
+++ src/main/java/com/jas/agent/Main.java
@@ -5,8 +5,6 @@ import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
 
 import java.lang.instrument.Instrumentation;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -19,36 +17,17 @@ public class Main {
         launch(args, inst);
     }
 
-    public static void agentmain(String args, Instrumentation inst) throws Exception {
-        launch(args, inst);
-    }
-
     private static void launch(String args, Instrumentation inst) throws Exception {
         System.out.println("rainbow-brackets-2024.2.1 cracked plugin");
 
         AgentBuilder agentBuilder = new AgentBuilder.Default()
-                .ignore(ElementMatchers.none()) // 忽略空，即允许 hook 所有类
-                .with(AgentBuilder.RedefinitionStrategy.REDEFINITION) // 开启类被加载后也允许进行字节码修改
-                .with(AgentBuilder.Listener.StreamWriting.toSystemError().withErrorsOnly()) // 字节码修改失败打印错误信息到控制台
-                .with(AgentBuilder.Listener.StreamWriting.toSystemOut().withTransformationsOnly()) // 字节码修改成功也输出到控制台
-                .with(new DumpClassListener()); // 字节码修改成功把类信息给报错到 weaving/classes 目录下
-
+                .ignore(ElementMatchers.none())
+                .with(AgentBuilder.RedefinitionStrategy.REDEFINITION);
         byPassJavaAgent(agentBuilder, inst);
-
         byPassLicense(agentBuilder, inst);
     }
 
-    /**
-     * 在 getUserOptionsFile 获取配置文件时返回默认的文件位置，会检测文件中是否包含 ja
-     * <p>
-     * 在 StringUtils.isEmpty 方法中，如果不是 com.janetfilter 包下的类调用，则抛出异常，因为检测了这个方法
-     */
     private static void byPassJavaAgent(AgentBuilder agentBuilder, Instrumentation inst) {
-        agentBuilder.type(ElementMatchers.named("com.intellij.diagnostic.VMOptions"))
-                .transform(((builder, typeDescription, classLoader, module, protectionDomain) ->
-                        builder.visit(Advice.to(VMOptionsInterceptor.class).on(ElementMatchers.named("getUserOptionsFile"))))
-                ).installOn(inst);
-
         agentBuilder.type(ElementMatchers.named("com.janetfilter.core.utils.StringUtils"))
                 .transform(((builder, typeDescription, classLoader, javaModule, protectionDomain) ->
                         builder.visit(Advice.to(StringUtilsInterceptor.class).on(ElementMatchers.named("isEmpty")))))
@@ -57,11 +36,16 @@ public class Main {
                 .transform(((builder, typeDescription, classLoader, module, protectionDomain) ->
                         builder.visit(Advice.to(ClassForNameInterceptor.class).on(ElementMatchers.named("forName")))))
                 .installOn(inst);
+    }
 
-//        agentBuilder.type(ElementMatchers.named("ڇଅ"))
-//                .transform(((builder, typeDescription, classLoader, javaModule, protectionDomain) ->
-//                        builder.visit(Advice.to(ReturnBoolInterceptor.class).on(ElementMatchers.named("ڥզ").and(ElementMatchers.returns(boolean.class))))))
-//                .installOn(inst);
+    /**
+     * 设置证书过期时间为 50 天，绕过大于 60 天的检测
+     */
+    private static void byPassLicense(AgentBuilder agentBuilder, Instrumentation inst) {
+        agentBuilder.type(ElementMatchers.named("com.intellij.ui.LicensingFacade"))
+                .transform((builder, typeDescription, classLoader, javaModule, protectionDomain) ->
+                        builder.visit(Advice.to(LicenseExpirationInterceptor.class).on(ElementMatchers.named("getLicenseExpirationDate"))))
+                .installOn(inst);
     }
 
     public static class StringUtilsInterceptor {
@@ -79,52 +63,21 @@ public class Main {
         }
     }
 
-    public static class VMOptionsInterceptor {
-
-        @Advice.OnMethodExit
-        public static void interceptor(@Advice.Return(readOnly = false) Path ret) {
-            try {
-                if (new Throwable().getStackTrace()[2].getClassName().startsWith("jdk.internal.reflect")) {
-                    String fileName = (String) Class.forName("com.intellij.diagnostic.VMOptions").getDeclaredMethod("getFileName").invoke(null);
-                    String location = (String) Class.forName("com.intellij.openapi.application.PathManager").getDeclaredMethod("getBinPath").invoke(null);
-                    ret = Paths.get(location, fileName);
-                }
-            } catch (Exception e) {
-                // ignore
-            }
-        }
-    }
-
     public static class ClassForNameInterceptor {
         @Advice.OnMethodEnter
         public static void interceptorBefore(@Advice.AllArguments Object[] args) {
-            if (args[0] != null && args[0].toString().startsWith("jdk.internal.org.objectweb.asm")) {
-//                System.out.println(Arrays.toString(new Throwable().getStackTrace()));
-                throw new RuntimeException("asm 用来修改字节码的");
+            String[] black = new String[]{
+                    "jdk.internal.org.objectweb.asm.",
+                    "com.intellij.diagnostic.VMOptions"
+            };
+            if (args[0] != null) {
+                for (String str : black) {
+                    if (args[0].toString().startsWith(str)) {
+                        throw new RuntimeException("fuck off " + args[0].toString());
+                    }
+                }
             }
         }
-    }
-
-    public static class ReturnBoolInterceptor {
-        @Advice.OnMethodExit
-        public static void interceptor(@Advice.Return(readOnly = false) boolean ret) {
-            try {
-                ret = false;
-            } catch (Exception e) {
-                // ignore
-            }
-        }
-    }
-
-
-    /**
-     * 设置证书过期时间为 50 天，绕过大于 60 天的检测
-     */
-    private static void byPassLicense(AgentBuilder agentBuilder, Instrumentation inst) {
-        agentBuilder.type(ElementMatchers.named("com.intellij.ui.LicensingFacade"))
-                .transform((builder, typeDescription, classLoader, javaModule, protectionDomain) ->
-                        builder.visit(Advice.to(LicenseExpirationInterceptor.class).on(ElementMatchers.named("getLicenseExpirationDate"))))
-                .installOn(inst);
     }
 
     public static class LicenseExpirationInterceptor {
